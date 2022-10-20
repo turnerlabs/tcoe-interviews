06a8b498aa16fd883b40d97e68a3ff2236610d2b
maoalejandro <mauricio.parra@smiledirectlcub.com>
2022-10-20 11:47:54 -0500

    [ADD] - API test added: Request: The API should receive dates in format d-mmmm-yyyy [ADD] - API test added: Request & Bug: The API should not receive dates in format different than d-mmmm-yyyy [ADD] - API test added: Response: The API should response for the date 5-January-2001 with a date field [ADD] - API test added: Response & Bug: The API should response for the date 5-January-2001 with a open, high, low and close number fields [ADD] - API test added: Negative & Bug: The API should not receive days with a leading zeroes [ADD] - API test added: Negative & Boundary & Bug: The API should not receive non calendar days

--- scenarios/stocksapi/test/trade.js
+++ scenarios/stocksapi/test/trade.js
@@ -1,21 +1,79 @@
 var chai = require('chai')
-  , chaiHttp = require('chai-http')
-  , app = require('../api/app');
+    , chaiHttp = require('chai-http')
+    , app = require('../api/app');
 
 var expect = require('chai').expect;
+
+const endpoint = '/api/stocks'
 
 chai.use(chaiHttp);
 
 //information on how to use chaijs and chai-http: https://www.chaijs.com/plugins/chai-http/
 
 describe('stocks api', function () {
-  describe('trades', function () {
-    it('should be happy', async function () {
-      return chai.request(app)
-        .get('/api/stocks')
-        .then(function (res) {
-          expect(res).to.have.status(200);
-        })
+    describe('trades', function () {
+        it('should be happy', async function () {
+            return chai.request(app)
+                .get(endpoint)
+                .then(function (res) {
+                    expect(res).to.have.status(200);
+                })
+        });
+        it('Request: The API should receive dates in format d-mmmm-yyyy', async function () {
+            const date = '20-October-2023'
+            return chai.request(app)
+                .get(endpoint + '?date=' + date)
+                .then(function (res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body.date).equal(date);
+                })
+        });
+        it('Request & Bug: The API should not receive dates in format different than d-mmmm-yyyy', async function () {
+            const date = 'october-10-2021'
+            return chai.request(app)
+                .get(endpoint + '?date=' + date)
+                .then(function (res) {
+                    expect(res).to.have.status(400);
+                })
+        });
+        it('Response: The API should response for the date 5-January-2001 with a date field', async function () {
+            const date = '5-January-2001'
+            return chai.request(app)
+                .get(endpoint + '?date=' + date)
+                .then(function (res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body.date).equal(date);
+                    expect(res.body.date).to.be.an('string');
+                })
+        });
+        it('Response & Bug: The API should response for the date 5-January-2001 with a open, high, ' +
+            'low and close number fields', async function () {
+            const date = '5-January-2001'
+            return chai.request(app)
+                .get(endpoint + '?date=' + date)
+                .then(function (res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body.open).to.be.an('number');
+                    expect(res.body.high).to.be.an('number');
+                    expect(res.body.low).to.be.an('number');
+                    expect(res.body.close).to.be.an('number');
+                })
+        });
+        it('Negative & Bug: The API should not receive days with a leading zeroes', async function () {
+            const date = '05-January-2001'
+            return chai.request(app)
+                .get(endpoint + '?date=' + date)
+                .then(function (res) {
+                    expect(res).to.have.status(400);
+                })
+        });
+      it('Negative & Boundary & Bug: The API should not receive non calendar days', async function () {
+        const date = '32-January-2001'
+        return chai.request(app)
+            .get(endpoint + '?date=' + date)
+            .then(function (res) {
+              expect(res).to.have.status(400);
+            })
+      });
     });
-  });
 });
