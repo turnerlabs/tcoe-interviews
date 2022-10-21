24e636f5443fae6eaba0d8b276840cd35792c79e
lawilangat <lawi.langat@warnermedia.com>
2022-10-21 15:08:47 -0400

    Added mobile android playground

--- scenarios/androidplayground/test/constants.js
+++
@@ -1,6 +0,0 @@
-export const DEFAULT_TIMEOUT = 20000;
-export const EXTENDED_TIMEOUT = 30000;
-export const THREE_MIN_TIMEOUT = 180000;
-export const FIFTEEN_MIN_TIMEOUT = 900000;
-export const ACCOUNT_VERIFICATION_REMINDER =
-  "Your account is not verified. To ensure you’re able to receive important account information, please check your e-mail to verify your address. Resend verification link.";
