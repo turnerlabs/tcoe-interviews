f40bc484069164afac1e9b69c6d347fd127840f3
sumitparmarsride <sumit.parmar@sride.co>
2022-10-26 17:20:04 -0600

    added test challange

--- scenarios/stocksapi/test/trade.js
+++ scenarios/stocksapi/test/trade.js
@@ -17,5 +17,62 @@ describe('stocks api', function () {
           expect(res).to.have.status(200);
         })
     });
+
+    it('Request: Content type is application/json', async function () {
+      return chai.request(app)
+        .get('/api/stocks?date=5-January-2000')
+        .then(function (res) {
+          expect(res.type).to.have.contain('application/json')
+        })
+    });
   });
+
+  it('Response: Verify attribute of Response.', async function () {
+    return chai.request(app)
+      .get('/api/stocks?date=5-January-2000')
+      .then(function (res) {
+        console.log(res.body);
+        expect(res.body).to.have.property('date');
+        expect(res.body).to.have.property('open');
+        expect(res.body).to.have.property('high');
+        expect(res.body).to.have.property('low');
+        expect(res.body).to.have.property('close');
+      })
+  });
+
+  it('Response: Verify attribute values of Response.', async function () {
+    return chai.request(app)
+      .get('/api/stocks?date=5-January-2000')
+      .then(function (res) {
+        console.log(res.body);
+        expect(res.body).to.have.property('date').to.be.a('string');
+        expect(res.body).to.have.property('open').to.be.a('string');
+        expect(res.body).to.have.property('high').to.be.a('string');
+        expect(res.body).to.have.property('low').to.be.a('string');
+        expect(res.body).to.have.property('close').to.be.a('string');
+      })
+  });
+
+  it('Negative test case', async function () {
+    return chai.request(app)
+      .get('/api/stocks?date=5-January-2000')
+      .then(function (res) {
+        console.log(res.body);
+        expect(res.body).to.have.property('date').to.be.a('string');
+        expect(res.body).to.have.property('open').to.be.a('number');
+        expect(res.body).to.have.property('high').to.be.a('number');
+        expect(res.body).to.have.property('low').to.be.a('number');
+        expect(res.body).to.have.property('close').to.be.a('number');
+      })
+  });
+
+  it('boundry value: Validate day in the body is less than 32', async function () {
+    return chai.request(app)
+      .get('/api/stocks?date=5-January-2000')
+      .then(function (res) {
+        var day = parseInt('/api/stocks?date=5-January-2000'.split('=')[1].split('-')[0]);
+        expect(day).to.be.lessThan(32);
+      })
+  });
+
 });
--- scenarios/uiplayground/test/specs/example.e2e.js
+++ scenarios/uiplayground/test/specs/example.e2e.js
@@ -1,13 +1,28 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
+const ResourcesPage = require('../pageobjects/resources.page');
 
-describe('My Login application', () => {
+describe('UI Validation scenarios', () => {
     it('should login with valid credentials', async () => {
-        await LoginPage.open();
+        await LoginPage.open('login');
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
     });
+
+    it('Validate going to resources page', async () => {
+        await ResourcesPage.open('resources');
+        await expect(ResourcesPage.pageHeader).toBeExisting();
+        await expect(browser).toHaveUrlContaining('/resources');
+    });
+
+    it('Validate all heading of resources page', async () => {
+        await ResourcesPage.open('resources');
+        const count =  await ResourcesPage.resourcesHeaders.length;
+        console.log("count of total elements:- " + count);
+    });
+
+    
 });
