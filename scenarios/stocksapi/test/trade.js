ae63e164aaae05410986f851f0444dd63e768bc0
Oscar Raul Gonzalez Venzor <oscarraul.gonzalez@globant.com>
2022-10-27 15:22:00 -0600

    API Coding Challenge Oscar

--- scenarios/stocksapi/test/trade.js
+++ scenarios/stocksapi/test/trade.js
@@ -18,4 +18,74 @@ describe('stocks api', function () {
         })
     });
   });
+
+  describe('api/stocks', function () {
+    it('should return status code 200 when sending a date in the format d-mmmm-yyyy as a query parameter', async function () {
+      return chai.request(app)
+        .get('/api/stocks?date=5-January-2001')
+        .then(function (res) {
+          expect(res).to.have.status(200);
+        })
+    });
+
+    it('should return 404 status code with a "POST" request as that method is not defined', async function () {
+      return chai.request(app)
+        .post('/api/stocks?date=5-January-2001')
+        .then(function (res) {
+          expect(res).to.have.status(404);
+        })
+    });
+
+    it('should contain the properties("open", "high", "low" and "close") in the response body (Object)', async function () {
+      return chai.request(app)
+        .get('/api/stocks?date=5-January-2001')
+        .then(function (res) {
+          expect(res.body).to.be.an("object")
+          expect(res.body).to.have.property("open");
+          expect(res.body).to.have.property("high");
+          expect(res.body).to.have.property("low");
+          expect(res.body).to.have.property("close");
+        });
+    });
+
+    it('should return the same date that is sent as a query parameter', async function () {
+      return chai.request(app)
+        .get('/api/stocks?date=5-January-2001')
+        .then(function (res) {
+          expect(res.body).to.have.property("date").eql("5-January-2001")
+        });
+    });
+
+    it('should return 400 status code when sending an empty date as a query parameter as it should be needed to get the stock information', async function () {
+      return chai.request(app)
+        .get('/api/stocks?date=')
+        .then(function (res) {
+          expect(res).to.have.status(400);
+        });
+    });
+
+    it('should return 400 status code when no query parameter is sent as it should be needed to get the stock information', async function () {
+      return chai.request(app)
+        .get('/api/stocks/')
+        .then(function (res) {
+          expect(res).to.have.status(400);
+        });
+    });
+
+    it('should return 400 status code when sending a date format other than d-mmmm-yyyy', async function () {
+      return chai.request(app)
+        .get('/api/stocks?date=05-January-2001')
+        .then(function (res) {
+          expect(res).to.have.status(400);
+        });
+    });
+
+    it('should return status code 400 when sending data that is not in date format', async function () {
+      return chai.request(app)
+        .get('/api/stocks/?date=testing')
+        .then(function (res) {
+          expect(res).to.have.status(400);
+        });
+    });
+  });
 });
