07aa9facbc3653f20c3c0e2dfef82a2216e06cd7
Martín Octavio <108762386+Martin-Audeves@users.noreply.github.com>
2022-11-01 16:34:16 -0600

    Add files via upload

---
+++ scenarios/uiplayground/test/specs/exercise-1.js
@@ -0,0 +1,29 @@
+describe ('Ex 1', () => {
+    it('wait for the progress bar to reach 75%', async () => {
+    await browser.url('http://uitestingplayground.com/progressbar');
+    const startButton = $('#startButton');
+    const stopButton = $('#stopButton');
+    await startButton.click();
+    await browser.waitUntil(
+            async () => (await $('#progressBar').getText()) === '75%',
+            {
+                        timeout: 15000
+            }
+        );
+    await stopButton.click();
+    await browser.pause(5000);
+    });
+    });
+
+describe ('Ex 1.2', () => {
+    it('hide button shows unhide', async () => {
+    await browser.url('http://uitestingplayground.com/visibility');
+    const hideButton = $('#hideButton');
+    await hideButton.click();
+    await browser.waitUntil(
+            async () => (await hideButton.getText()) === 'unhide'
+        );
+    await stopButton.click();
+    await browser.pause(5000);
+    });
+    });
