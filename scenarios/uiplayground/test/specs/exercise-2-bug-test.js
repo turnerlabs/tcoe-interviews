dbd6bec7e8fb6d687380e383a344a87f1b4f1ebe
Abhishek Bhatt <abhishekqa09@gmail.com>
2022-11-10 00:27:48 -0500

    refactor test name for bug test

--- scenarios/uiplayground/test/specs/exercise-2-bug-test.js
+++ scenarios/uiplayground/test/specs/exercise-2-bug-test.js
@@ -2,7 +2,7 @@ const LoginPage = require('../pageobjects/login.page');
 const testData = require('../../testData/constants');
 
 describe('Bug test', () => {
-    it('should not see username and password field', async () => {
+    it('should not see username and password field after successful login', async () => {
         await LoginPage.open();
         await LoginPage.login(`${testData.validUser.userName}`,`${testData.validUser.password}`);
         await LoginPage.verifyWelcomeText(`${testData.validUser.userName}`);
