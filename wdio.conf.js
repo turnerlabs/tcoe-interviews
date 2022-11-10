e4bbf7fe2b44d4fd0a41356c701c1675d084780f
Abhishek Bhatt <abhishekqa09@gmail.com>
2022-11-10 00:22:34 -0500

    remove dot reporter

--- wdio.conf.js
+++ wdio.conf.js
@@ -131,7 +131,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec','dot'],
+    reporters: ['spec'],
 
 
     
