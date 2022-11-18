731d93b92b1692790a5ab2b4981da75e19987411
Cristian David <cristiandavid1291@gmail.com>
2022-11-18 16:17:00 -0500

    Initial commit and structure

---
+++ scenarios/uiplayground/test/specs/TS_001_Search_Functionality/TS_001_SF.js
@@ -0,0 +1,13 @@
+
+
+describe('TS_001_Search_Functionality', () => {
+
+    it('TC_SF_001', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
