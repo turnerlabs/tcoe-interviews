30d03de16c151312fb061af29c67478b513cb94e
Leonardo Bautista <Leonardo.Y.Bautista.-ND@disney.com>
2022-11-18 14:21:39 -0800

    [Interview-UI] - Resolving Exercise 1. Thank you!

--- scenarios/uiplayground/test/specs/exercise1.negative.ajax.js
+++ scenarios/uiplayground/test/specs/exercise1.negative.ajax.js
@@ -1,20 +1,20 @@
-// const Ajax = require('../pageobjects/Ajax');
-//
-// /**
-//  * Negative Cases
-//  */
-// describe('Exercise 1 - Negative tests cases', () => {
-//     beforeEach(async () => {
-//         await browser.url('ajax');
-//         await browser.setTimeout({ 'pageLoad': 10000 });
-//         await Ajax.buttonTriggeringAjaxRequest.waitForDisplayed();
-//     });
-//
-//     it('should appears ony one message by clicking more than once', async () => {
-//         await Ajax.buttonTriggeringAjaxRequest.click();
-//         await Ajax.clickingMultipleTimes()
-//         await Ajax.resultField.waitForDisplayed();
-//         await expect(await Ajax.message.nextElement() != null);
-//     });
-//
-// });
+const Ajax = require('../pageobjects/Ajax');
+
+/**
+ * Negative Cases
+ */
+describe('Exercise 1 - Negative tests cases', () => {
+    beforeEach(async () => {
+        await browser.url('ajax');
+        await browser.setTimeout({ 'pageLoad': 10000 });
+        await Ajax.buttonTriggeringAjaxRequest.waitForDisplayed();
+    });
+
+    it('should appears ony one message by clicking more than once', async () => {
+        await Ajax.buttonTriggeringAjaxRequest.click();
+        await Ajax.clickingMultipleTimes()
+        await Ajax.resultField.waitForDisplayed();
+        await expect(await Ajax.message.nextElement() != null);
+    });
+
+});
