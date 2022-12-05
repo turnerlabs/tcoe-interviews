d58365dae007598cd127e6a27092a198b0cd5af0
laura.cabezas <lauracs1213@gmail.com>
2022-12-05 12:03:34 -0500

    finish tests

--- config/wdio.android.app.conf.js
+++ config/wdio.android.app.conf.js
@@ -61,9 +61,9 @@ exports.config = {
         // For W3C the appium capabilities need to have an extension prefix
         // http://appium.io/docs/en/writing-running-appium/caps/
         // This is `appium:` for all Appium Capabilities which can be found here
-        'appium:deviceName': 'emulator-5554',
+        'appium:deviceName': 'eaa05f6a',
         'appium:platformVersion': '11.0',
-        //'appium:orientation': 'PORTRAIT',
+        'appium:orientation': 'PORTRAIT',
         'appium:automationName': 'UiAutomator2',
         // The path to the app
         'appium:app': './apps/Android-NativeDemoApp-0.4.0.apk',
