ca14e3dbe3ca8272f7e79bbd6b735537232143f7
laura.cabezas <lauracs1213@gmail.com>
2022-12-05 10:30:39 -0500

    add third test

--- tests/specs/app.forms.spec.js
+++ tests/specs/app.forms.spec.js
@@ -2,18 +2,33 @@ const HomeScreen = require('../screenobjects/HomeScreen');
 
 
 describe('Form components validation', () => {
- 
+
+    beforeEach(async () => {
+        await HomeScreen.waitForLoading();
+    });
+
+    afterEach(async () => {
+        await HomeScreen.goToHome();
+    });
+
     it('should be present the default selection of the tab', async () => {
         await HomeScreen.waitForLoading();
         await expect(HomeScreen.formsOption).toHaveAttribute('selected', 'false');
     });
-    
+
     it('form tab should be available for selection and is clickable', async () => {
         await expect(HomeScreen.formsOption).toBeDisplayed();
         await expect(HomeScreen.formsOption).toBeEnabled();
         await expect(HomeScreen.formsOption).toHaveAttribute('clickable', 'true');
     });
-   
+
+    it('form tab should change color when is selected', async () => {
+        await HomeScreen.goToForms();
+        await expect(HomeScreen.formsOption).toBeSelected();
+    });
+
+
+
 });
 
 
