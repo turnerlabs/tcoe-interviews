81e691943355b899761212a038ae1870d957441a
am-garc1a <am.garcia@globant.com>
2022-12-05 09:51:15 -0500

    input functionality test finished

--- test/specs/app.forms.spec.js
+++ test/specs/app.forms.spec.js
@@ -1,5 +1,6 @@
 const TabBar = require("../screenobjects/components/TabBar");
 const FormsScreen = require("../screenobjects/FormsScreen");
+const data = require("../../test_data_source/data.provider");
 
 describe("Interaction in Forms Screen", () => {
   beforeEach(async () => {
@@ -36,5 +37,26 @@ describe("Interaction in Forms Screen", () => {
     await expect(await driver.isKeyboardShown()).toBeTruthy();
     await driver.back();
     await expect(await driver.isKeyboardShown()).toBeFalsy();
+  });
+
+  it("should type on input and verify it work properly", async () => {
+    await FormsScreen.tapOnInput();
+    await FormsScreen.typeOnInput(data.text_with_special_characters.text);
+    await expect(await FormsScreen.textResultInput.getText()).toEqual(
+      data.text_with_special_characters.text
+    );
+
+    await FormsScreen.tapOnInput();
+    await FormsScreen.typeOnInput(data.text_which_exceeds_capacity.text);
+
+    await expect(
+      (
+        await FormsScreen.textResultInput.getText()
+      ).length
+    ).toBeLessThan(data.text_which_exceeds_capacity.text.length);
+
+    await expect(await FormsScreen.textResultInput.getText()).toHaveLength(
+      data.input_max_character
+    );
   });
 });
---
+++ test_data_source/data.provider.js
@@ -0,0 +1,12 @@
+module.exports = {
+  input_max_character: 30,
+  simple_text: {
+    text: "This is a text",
+  },
+  text_with_special_characters: {
+    text: "AKJasgHT9236987.!$_-/%&(/",
+  },
+  text_which_exceeds_capacity: {
+    text: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  },
+};
