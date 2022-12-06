94abb608d69cf0b43f71c48fd252b57d6df535df
mazher <mazher.syed@ontario.ca>
2022-12-05 21:24:51 -0500

    exercise3 - complete - mazher syed

--- scenarios/uiplayground/test/pageobjects/page.js
+++ scenarios/uiplayground/test/pageobjects/page.js
@@ -7,7 +7,7 @@ module.exports = class Page {
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    open (path) {
-        return browser.url(`http://uitestingplayground.com/dynamictable${path}`)
+     open(path = "") {
+        return browser.url(`http://uitestingplayground.com/dynamictable/${path}`);
     }
 }
