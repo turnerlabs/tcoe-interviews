26127e968d5c772b51b77430354f586ce49210e9
lucas <lucashdoa@gmail.com>
2022-12-15 13:57:25 -0300

    setup: initial project structure

---
+++ wdio.conf.js
@@ -0,0 +1,29 @@
+exports.config = {
+  runner: 'local',
+  specs: ['./test/specs/**/*.spec.js'],
+  // Patterns to exclude.
+  exclude: [
+    // 'path/to/excluded/files'
+  ],
+  maxInstances: 10,
+  capabilities: [
+    {
+      maxInstances: 1,
+      browserName: 'chrome',
+      acceptInsecureCerts: true,
+    },
+  ],
+  logLevel: 'info',
+  bail: 0,
+  baseUrl: 'https://uitestingplayground.com/',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  services: ['chromedriver'],
+  framework: 'mocha',
+  reporters: ['spec'],
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60000,
+  },
+};
