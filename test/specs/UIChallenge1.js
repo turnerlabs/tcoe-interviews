2ae6a15bf4b2a78e68f601a113096a2687990ae4
Utkarsh10792 <117960015+Utkarsh10792@users.noreply.github.com>
2023-01-11 15:16:27 -0300

    code formatting in Spec file

--- test/specs/UIChallenge1.js
+++ test/specs/UIChallenge1.js
@@ -6,51 +6,49 @@ import ButtonColor from '../pageobjects/buttonColor.js';
 
 describe("UI Challenge 1",async()=>{
 
-it('Progress bar start wait stop and tollerence', async () => {
+    it('Progress bar start wait stop and tollerence', async () => {
+        await ProgressPage.open()
+        await ProgressPage.startButton.click()
+        await ProgressPage.progressBar.waitUntil(async function() {
+            const tollerence = (await this.getAttribute('aria-valuenow'))
+            return ( tollerence >= '75' && tollerence <= '80' )
+        },
+        {
+            timeout: 50000,
+            timeoutmsg:"Failed while waiting for progressBar to reach 75%"
+        })
+        await ProgressPage.stopButton.click()
+    });
 
-    await ProgressPage.open()
-    await ProgressPage.startButton.click(); 
-    await ProgressPage.progressBar.waitUntil(async function() {
-        const tollerence = (await this.getAttribute('aria-valuenow'))
-        return ( tollerence >= '75' && tollerence <= '80' )
-    },
-    {
-        timeout: 50000,
-        timeoutmsg:"Failed while waiting for progressBar to reach 75%"
+    it('Ajax to validate text is displayed only once', async () => {
+        await AjaxPage.open()
+        await AjaxPage.ajaxClick(2)
+        const numberOfTimesMsgDisp = await AjaxPage.ajaxSuccess.length
+        expect(numberOfTimesMsgDisp).toEqual(1)
+    });
+
+    it('Hide and Unhide button test', async () => {
+        await HidePage.open()
+        await HidePage.hideButton.click()
+        let btnText = await HidePage.unHideButton.getText()
+        expect(btnText.toLowerCase()).toEqual('unhide'.toLowerCase())
+    });
+
+    it('Copy button test validation', async () => {
+        await ShadowCopy.open()
+        await ShadowCopy.generatorText()
+        await ShadowCopy.copyText()
+        let generatedText = await ShadowCopy.saveText()
+        let pastedText = await ShadowCopy.pasteText()
+        expect(pastedText).toEqual(generatedText)
+    });
+
+    it('Button turns red validation', async () => {
+        await ButtonColor.open()    
+        await ButtonColor.badButton.click()
+        const colorOfButton =  await ButtonColor.badButton.getCSSProperty('background-color')
+        const colorOfButtonActual = colorOfButton.value
+        expect(colorOfButtonActual).toEqual("rgba(255,0,0,1)")
     });    
-    await ProgressPage.stopButton.click();
-});
-
-it('Ajax to validate text is displayed only once', async () => {
-    await AjaxPage.open()
-    await AjaxPage.ajaxClick(2)
-    const numberOfTimesMsgDisp = await AjaxPage.ajaxSuccess.length
-    expect(numberOfTimesMsgDisp).toEqual(1)
-});
-
-it('Hide and Unhide button test', async () => {
-    await HidePage.open()
-    await HidePage.hideButton.click()
-    let btnText = await HidePage.unHideButton.getText()
-    expect(btnText.toLowerCase()).toEqual('unhide'.toLowerCase())
-});
-
-it('Copy button test validation', async () => {
-    await ShadowCopy.open()
-    await ShadowCopy.generatorText()
-    await ShadowCopy.copyText()
-    let generatedText = await ShadowCopy.saveText()
-    let pastedText = await ShadowCopy.pasteText()
-    expect(pastedText).toEqual(generatedText)
-});
-
-it('Button turns red validation', async () => {
-    await ButtonColor.open()    
-    await ButtonColor.badButton.click()
-    const colorOfButton =  await ButtonColor.badButton.getCSSProperty('background-color')
-    const colorOfButtonActual = colorOfButton.value
-    expect(colorOfButtonActual).toEqual("rgba(255,0,0,1)")
-});    
-    
-    
+  
 })
